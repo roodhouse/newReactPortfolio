1e886621ef5d2c2125c6dc889faa98682a5fa5eb
roodhouse <rughjm@gmail.com>
2022-12-28 10:01:09 -0600

    about component

--- rugh-portfolio/src/App.js
+++ rugh-portfolio/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Header from '../src/components/Header/Header'
+import About from '../src/components/About/About'
+import Portfolio from './components/Portfolio/Portfolio'
+import Contact from '../src/components/Contact/Contact'
+
 function App() {
   return (
     <>
     <Header />
+    <About />
+    <Portfolio />
+    {/* <Contact /> */}
     </>
   );
 }
---
+++ rugh-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div className='pt-5 mx-5'>
+        <h2>Portfolio</h2>
+        <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
