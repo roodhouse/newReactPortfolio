1407912110c124c2c895b34bc31d88e3d1ff8536
roodhouse <rughjm@gmail.com>
2022-12-28 10:18:07 -0600

    portfolio section

--- rugh-portfolio/src/components/Portfolio/Portfolio.jsx
+++ rugh-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import './portfolio.css'
+import Awesome from '../../assets/awesomeSauce.jpg';
+import Cheat from '../../assets/cheatsheet.png';
+import Wheel from '../../assets/color-wheel-icon.svg'
+import Freedom from '../../assets/freedomsCost.jpg'
 
 const Portfolio = () => {
   return (
     <div className='pt-5 mx-5'>
         <h2>Portfolio</h2>
-        <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+        <div className='d-flex flex-wrap'>
+            <img className='pImg' src={Awesome} alt="" />
+            <img className='pImg' src={Cheat} alt="" />
+            <img className='pImg' src={Wheel} alt="" />
+            <img className='pImg' src={Freedom} alt="" />
         </div>
     </div>
   )
