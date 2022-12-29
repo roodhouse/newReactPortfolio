dcdf0c87ce42c32dce51c1d1b8658ce5d8a8edfa
roodhouse <rughjm@gmail.com>
2022-12-29 11:03:35 -0600

    integrated form to show an alert

--- rugh-portfolio/src/components/Contact/Contact.jsx
+++ rugh-portfolio/src/components/Contact/Contact.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    return name === 'userName' ? setUserName(value) : setUserEmail(value);
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`BAM! ${userName} your email is ${userEmail}`);
+    setUserName('');
+    setUserEmail('');
+  }
+
   return (
     <div id='contact' className='pt-5 mx-5'>
       <h2>Contact Me</h2>
       <form action="">
         <ul>
           <li>
-            <label for='name'>Name:</label>
-            <input type="text" id='name' name='user-name' />
+            <label>Name:</label>
+            <input type="text" id='name' name='userName' onChange={handleInputChange} value={userName} />
           </li>
           <li>
-            <label for='email'>Email:</label>
-            <input type="text" id='email' name='user-email' />
+            <label>Email:</label>
+            <input type="text" id='email' name='userEmail' onChange={handleInputChange} value={userEmail} />
           </li>
           <li>
-            <button type='submit'>Submit</button>
+            <button type='submit' onClick={handleFormSubmit}>Submit</button>
           </li>
         </ul>
       </form>
